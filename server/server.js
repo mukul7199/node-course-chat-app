20758f5686e91a5cdf062488e8e0d06625cd1c63
mukul7199 <mukul7199@gmail.com>
2018-12-11 18:58:47 +0530

    date format changed to string

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            createdAt: new Date().getTime()
+            createdAt: new Date().getTime().toString()
         })
     });
 
